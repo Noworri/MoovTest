8fe27efb7762d85a80c2875f8644eecfcd284848
RichGadjPAL <richard@pals.africa>
2024-01-17 15:43:17 +0000

    Message de commit explicatif

---
+++ app/Helpers/MtnMomo/Collection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helpers\MtnMomo;
+
+class Collection extends Mtn_Momo
+{
+    protected $_base_url = "https://sandbox.momodeveloper.mtn.com";
+
+    //collection
+    public function colToken()
+    {
+         
+    }
+
+    public function colRequestToPay($amount, $number, $currency)
+    {
+         
+    }
+
+    public function colStatus()
+    {
+        
+    }
+
+    public function colCheckBalance()
+    {
+        
+    }
+
+    public function colCheckAccountHolder($accountHolderId)
+    {
+        
+    }
+}
